17db155cb96c1acdffd0776e07a2453d5bf8b22b
Nrulafind <79900731+Nrulafind@users.noreply.github.com>
2023-06-12 22:29:02 +0700

    Delete routes directory

--- routes/routeChat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticate } = require('../middleware/authMiddleware');
-const { sendMessage } = require('../handlers/handlerChat');
-
-// Send message route using post
-router.post('/sendMessage', authenticate, sendMessage);
-
-module.exports = router;
--- routes/routeNotification.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticate } = require('../middleware/authMiddleware');
-const { sendNotification } = require('../handlers/handlerNotification');
-
-// Send notification route
-router.post('/sendNotification', authenticate, sendNotification);
-
-module.exports = router;
