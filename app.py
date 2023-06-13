dc32ab9206db787b3aa5362695ea4ce7caea12ad
Nrulafind <79900731+Nrulafind@users.noreply.github.com>
2023-06-13 18:58:19 +0700

    Update another ver app.py

--- app.py
+++ app.py
@@ -19,16 +19,16 @@ model = tf.keras.models.load_model(model_path)
 @app.route('/api/predict', methods=['POST'])
 def predict():
     input_data = request.json.get('input_data')
-    if input_data is None:
-        return jsonify({'message': 'input_data parameter is missing'}), 400
+    if input_data is None or len(input_data) != 4:
+        return jsonify({'message': 'input_data must contain 4 values'}), 400
     try:
-        input_tensor = tf.convert_to_tensor(input_data, dtype=tf.float64)
-        input_tensor = tf.reshape(input_tensor, [len(input_data), len(input_data[0])])
+        input_tensor = tf.convert_to_tensor([input_data], dtype=tf.float64)
         output_tensor = model.predict(input_tensor)
         results = output_tensor.tolist()
         return jsonify({'results': results}), 200
     except Exception as e:
         return jsonify({'error': 'Something went wrong'}), 500
+
 
 
 if __name__ == '__main__':
