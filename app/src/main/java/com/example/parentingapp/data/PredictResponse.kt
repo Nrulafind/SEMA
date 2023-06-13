8a10fc2e84160774c8eae3272dcbc9956ec39871
hildakhoirotul <2041720161@student.polinema.ac.id>
2023-06-13 19:30:24 +0700

    ambil prediksi dari api

--- app/src/main/java/com/example/parentingapp/data/PredictResponse.kt
+++ app/src/main/java/com/example/parentingapp/data/PredictResponse.kt
@@ -1,10 +1,18 @@
 package com.example.parentingapp.data
 
+import kotlinx.parcelize.Parcelize
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
+@Parcelize
 data class PredictResponse(
 
-//    val value: Double
-    @field:SerializedName("value")
-    var value : Double,
+	@field:SerializedName("results")
+	val results: Double,
+
+) : Parcelable
+
+
+data class Input(
+	val input_data: MutableList<Double>,
 )
