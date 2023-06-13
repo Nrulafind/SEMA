bb29392fe45f0da6223eb26a92daabe0647db3cd
Silvia Prada Aprilia <silviaprada0904@gmail.com>
2023-06-13 09:50:07 +0700

    Add Attendance Item

--- app/src/main/java/com/example/parentingapp/adapter/AttendanceAdapter.kt
+++ app/src/main/java/com/example/parentingapp/adapter/AttendanceAdapter.kt
@@ -3,9 +3,10 @@ package com.example.parentingapp.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.parentingapp.data.Attendance
 import com.example.parentingapp.databinding.ItemAttendanceBinding
 
-class AttendanceAdapter(): RecyclerView.Adapter<AttendanceAdapter.MyViewHolder>() {
+class AttendanceAdapter(private var listAttendance: ArrayList<Attendance>): RecyclerView.Adapter<AttendanceAdapter.MyViewHolder>() {
 
     class MyViewHolder(var binding: ItemAttendanceBinding): RecyclerView.ViewHolder(binding.root){
 
@@ -16,12 +17,12 @@ class AttendanceAdapter(): RecyclerView.Adapter<AttendanceAdapter.MyViewHolder>(
         return MyViewHolder(binding)
     }
 
-    override fun getItemCount(): Int = 10
+    override fun getItemCount(): Int = listAttendance.size
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-//        val newsData = listNews[position]
-//        holder.binding.ivNews.setImageResource(newsData.image)
-//        holder.binding.tvNewsTitle.text = newsData.title
-//        holder.binding.tvNewsDescription.text = newsData.description
+        val attendanceData = listAttendance[position]
+        holder.binding.tvDate.text = attendanceData.tanggal
+        holder.binding.tvDay.text = attendanceData.nama
+        holder.binding.tvAttendance.text = attendanceData.keterangan
     }
 }
--- app/src/main/java/com/example/parentingapp/ui/DetailAttendanceActivity.kt
+++ app/src/main/java/com/example/parentingapp/ui/DetailAttendanceActivity.kt
@@ -3,6 +3,12 @@ package com.example.parentingapp.ui
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.parentingapp.adapter.AttendanceAdapter
+import com.example.parentingapp.adapter.PostAdapter
+import com.example.parentingapp.adapter.ScoreAdapter
+import com.example.parentingapp.data.Attendance
+import com.example.parentingapp.data.Score
 import com.example.parentingapp.databinding.ActivityDetailAttendanceBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -13,6 +19,7 @@ class DetailAttendanceActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailAttendanceBinding
     private lateinit var auth: FirebaseAuth
+    private var listAttendance: ArrayList<Attendance> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +31,10 @@ class DetailAttendanceActivity : AppCompatActivity() {
         auth = Firebase.auth
         val user = auth.currentUser
 
+        binding.rvAttendance.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvAttendance.layoutManager = layoutManager
+
         val db = Firebase.firestore
         val docRef = db.collection("student")
             .document(user!!.uid)
@@ -33,7 +44,11 @@ class DetailAttendanceActivity : AppCompatActivity() {
         docRef.get()
             .addOnSuccessListener { document ->
                 if (document != null) {
+
                     binding.studentName.text = document.data!!["name"].toString()
+                    binding.studentGrade.text = document.data!!["class"].toString()
+                    binding.studentSemester.text = document.data!!["angkatan"].toString()
+
                     Log.d(TAG, "DocumentSnapshot data: ${document.data}")
                 } else {
                     Log.d(TAG, "No such document")
@@ -41,6 +56,60 @@ class DetailAttendanceActivity : AppCompatActivity() {
             }
             .addOnFailureListener { exception ->
                 Log.d(TAG, "get failed with ", exception)
+            }
+
+        val attendanceRef = db.collection("student")
+            .document(user!!.uid)
+            .collection("attendance")
+            .document(user!!.uid)
+
+        attendanceRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+
+                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
+
+            .addOnCompleteListener {
+                var attend: Attendance
+                if (it.isSuccessful) {
+                    val data = it.result.data
+                    data?.let {
+                        for ((_, value) in data) {
+                            val v = value as Map<*, *>
+                            val name = v["Nama"]
+                            val tanggal = v["Tanggal"]
+                            val keterangan = v["Keterangan"]
+                            attend = Attendance(
+                                "$name",
+                                "$tanggal",
+                                "$keterangan"
+                            )
+                            listAttendance.add(attend)
+//                            Log.d("TAG", "$score")
+//                            v.mapValues { it ->
+//                                val (name, score, kkm) = it.value as List<*>
+//
+//                                Score(
+//                                    name as String,
+//                                    score as String,
+//                                    kkm as String
+//                                )
+//
+//                                listScore.add(Score(name, score, kkm))
+//                            }
+                        }
+                    }
+                    Log.d("TAG", "$listAttendance")
+                    val adapter = AttendanceAdapter(listAttendance)
+                    binding.rvAttendance.adapter = adapter
+                }
             }
     }
 
