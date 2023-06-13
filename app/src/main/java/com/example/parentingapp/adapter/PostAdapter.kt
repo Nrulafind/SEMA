e65d67a994b97228922df1d15e834cbdfa3125c7
Silvia Prada Aprilia <silviaprada0904@gmail.com>
2023-06-13 19:46:27 +0700

    Update Post

--- app/src/main/java/com/example/parentingapp/adapter/PostAdapter.kt
+++ app/src/main/java/com/example/parentingapp/adapter/PostAdapter.kt
@@ -5,22 +5,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.parentingapp.R
+import com.example.parentingapp.data.Post
+import com.example.parentingapp.databinding.ItemNewsBinding
+import com.example.parentingapp.databinding.ItemPostBinding
 
-class PostAdapter: RecyclerView.Adapter<PostAdapter.MyViewHolder>() {
+class PostAdapter(private var listPost: ArrayList<Post>): RecyclerView.Adapter<PostAdapter.MyViewHolder>() {
 
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    class MyViewHolder(var binding: ItemPostBinding): RecyclerView.ViewHolder(binding.root){
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false)
-        return MyViewHolder(view)
+        val binding = ItemPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return 25
-    }
+    override fun getItemCount(): Int = listPost.size
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val postData = listPost[position]
+        holder.binding.ivPost.setImageResource(postData.image)
+        holder.binding.ivTeacher.setImageResource(postData.teacherPhoto)
+        holder.binding.tvTeacher.text = postData.teacherName
+        holder.binding.tvTimeUpload.text = postData.time
     }
 }
