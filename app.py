5b622d4514463e72d467ff5813437dec681349bd
Nrulafind <79900731+Nrulafind@users.noreply.github.com>
2023-06-13 14:54:58 +0700

    try another ver Update app.py

--- app.py
+++ app.py
@@ -18,18 +18,18 @@ model = tf.keras.models.load_model(model_path)
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    input_data = request.json['input_data']
-    if input_data is None: return jsonify({'message':'need input data'}), 401
+    input_data = request.json.get('input_data')
+    if input_data is None:
+        return jsonify({'message': 'input_data parameter is missing'}), 400
     try:
         input_tensor = tf.convert_to_tensor(input_data, dtype=tf.float64)
-        input_tensor = tf.reshape(input_tensor, [1, 4])
+        input_tensor = tf.reshape(input_tensor, [len(input_data), len(input_data[0])])
         output_tensor = model.predict(input_tensor)
         results = output_tensor.tolist()
-        #print(results[0][0])
-        return jsonify({'results': results[0][0]}), 200
-        #return json.loads(results), 200
+        return jsonify({'results': results}), 200
     except Exception as e:
         return jsonify({'error': 'Something went wrong'}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
