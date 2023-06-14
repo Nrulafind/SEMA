0d3028bf76f96db92e05e23a2e945440382d2ec4
hildakhoirotul <2041720161@student.polinema.ac.id>
2023-06-14 21:19:08 +0700

    get response from api

--- app/src/main/java/com/example/parentingapp/ui/ScoreDetailActivity.kt
+++ app/src/main/java/com/example/parentingapp/ui/ScoreDetailActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.parentingapp.R
 import com.example.parentingapp.adapter.CourseAdapter
 import com.example.parentingapp.adapter.ScoreAdapter
 import com.example.parentingapp.data.Input
@@ -23,24 +24,27 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import cz.msebera.android.httpclient.Header
 import cz.msebera.android.httpclient.entity.StringEntity
+import cz.msebera.android.httpclient.message.BasicHeader
+import cz.msebera.android.httpclient.protocol.HTTP
+import okhttp3.RequestBody
 import org.json.JSONArray
 import org.json.JSONObject
 import retrofit2.http.Headers
+import java.io.UnsupportedEncodingException
 
 @Suppress("DEPRECATION")
 class ScoreDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityScoreDetailBinding
     private lateinit var auth: FirebaseAuth
-    private val viewModel: ScoreViewModel by viewModels()
     var db = Firebase.firestore
-    var listInput: ArrayList<Any> = ArrayList(4)
+    var listInput: ArrayList<Double> = ArrayList(4)
+    private var predict: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityScoreDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val input = Input(listInput)
         val course = intent.getStringExtra(EXTRA_COURSE)
 
         binding.courseName.text = course
@@ -60,54 +64,21 @@ class ScoreDetailActivity : AppCompatActivity() {
         score.get()
             .addOnSuccessListener {
                 if (it != null) {
-//                    Log.d("success", "DocumentSnapshot data: ${it.data}")
                     binding.studentName.text = it.data?.get("name") as String
                     binding.studentGrade.text = it.data?.get("class") as String
                     binding.studentSemester.text = it.data?.get("angkatan") as String
                     getData(userId, course)
-//                    viewModel.getPredict(listInput)
                 } else {
                     Log.d("failed", "Data doesn't exist")
                 }
             }
         Log.d("TAG", listInput.toString())
-
-//        viewModel.predict.observe(this) { data->
-//            binding.status.text = data.results.toString()
-//        }
-//        val score = db.collection("student/$userId/userData").whereEqualTo("userData", userId)
-//        score.get()
-//            .addOnSuccessListener {
-//                if (it != null) {
-//                    binding.studentName.text = it.documents[0].data?.get("name") as String
-//                    binding.studentGrade.text = it.documents[0].data?.get("class") as String
-//                    binding.studentSemester.text = it.documents[0].data?.get("angkatan") as String
-//                }
-//            }
-//        score.get()
-//            .addOnCompleteListener{
-//                if (FirebaseAuth.getInstance().currentUser != null) {
-//                    binding.studentName.text = it.result.get("name").toString()
-//                }
-//            }
-
-//        getData(userId)
     }
 
     private fun getData(userId: String, course: String?) {
         var listScore: ArrayList<Score> = ArrayList()
-//        var input1: ArrayList<Int> = ArrayList()
-//        var input2: ArrayList<Int> = ArrayList()
-//        var avg1: Double
-//        var avg2: Double
         db.collection("student").document(userId).collection("Score").document(userId).collection(course.toString()).document(userId)
             .get()
-//            .addOnSuccessListener { results ->
-//                var listScore: ArrayList<Score> = ArrayList()
-//                if ( resul) {
-
-//                }
-//            }
             .addOnCompleteListener {
                 var score: Score
                 if (it.isSuccessful) {
@@ -123,55 +94,22 @@ class ScoreDetailActivity : AppCompatActivity() {
                                 "$nilai",
                                 "$kkm"
                             )
-//                            if (name == "Ulangan Harian 1" || name == "Ulangan Harian 2" || name == "Ulangan Harian 3"){
-//                                input1.add(nilai.toString().toInt())
-//                            }
-//                            if (name == "Ulangan Harian 4" || name == "Ulangan Harian 5" || name == "Ulangan Harian 6"){
-//                                input2.add(nilai.toString().toInt())
-//                            }
                             if (name == "UTS" || name == "UAS") listInput.add(nilai.toString().toDouble())
                             listScore.add(score)
-//                            Log.d("TAG", "$score")
-//                            v.mapValues { it ->
-//                                val (name, score, kkm) = it.value as List<*>
-//
-//                                Score(
-//                                    name as String,
-//                                    score as String,
-//                                    kkm as String
-//                                )
-//
-//                                listScore.add(Score(name, score, kkm))
-//                            }
                         }
                     }
-//                    avg1 = input1.average()
-//                    avg2 = input2.average()
-//                    listInput.add(avg1.toInt())
-//                    listInput.add(avg2.toInt())
-//                    binding.daftarNilai.text = listInput[0].toString()
-//                    listInput.add(95)
-//                    listInput.add(100)
-                    listInput.add(1)
-                    listInput.add(0)
+                    listInput.add(1.0)
+                    listInput.add(0.0)
                     Log.d("TAG", listInput.toString())
                     val client = AsyncHttpClient()
                     val url = "https://backendsema-x6blq7wjaa-et.a.run.app/api/predict"
 
-//                    val params = RequestParams(listInput)
-//                    params.put("input_data", listInput)
-
                     val array = JSONArray(listInput)
-                    val jsonString = array.toString()
-                    val params = StringEntity(jsonString)
 
-//                    val params = RequestParams()
-//                    params.put("input_data", listInput)
+                    val params = RequestParams()
+                    params.put("input_data", array)
 
-//                    client.addHeader("Content-Type", "application/json")
-
-                    params.setContentType("application/json")
-                    client.post(applicationContext, url, params, "application/json", object : AsyncHttpResponseHandler() {
+                    client.post(url, params, object : AsyncHttpResponseHandler() {
                         override fun getUseSynchronousMode(): Boolean {
                             return false
                         }
@@ -185,8 +123,18 @@ class ScoreDetailActivity : AppCompatActivity() {
                             Log.d("TAG", result)
                             try {
                                 val responseObject = JSONObject(result)
-                                val quote = responseObject.getDouble("results")
-                                binding.status.text = quote.toString()
+                                val predict = responseObject.getJSONArray("results")
+                                val predict2: JSONArray = predict.get(0) as JSONArray
+                                val status = predict2.get(0).toString().toDouble()
+//                                predict = quote2.getDouble(0)
+//                                binding.status.text = predict.toString()
+                                if (status >= 80.0){
+//                                    binding.status.text = predict2.get(0).toString()
+                                    binding.status.text = "Siswa Berprestasi"
+                                } else {
+                                    binding.status.text = "Memerlukan Tindakan"
+                                }
+
                             } catch (e: Exception) {
                                 Toast.makeText(this@ScoreDetailActivity, e.message, Toast.LENGTH_SHORT).show()
                                 e.printStackTrace()
@@ -215,12 +163,6 @@ class ScoreDetailActivity : AppCompatActivity() {
                     binding.rvScore.adapter = adapter
                 }
             }
-//        Log.d("TAG", listInput.toString())
-
-//        viewModel.getPredict(listInput)
-//        viewModel.predict.observe(this) { data->
-//            binding.status.text = data.results.toString()
-//        }
     }
 
     companion object {
