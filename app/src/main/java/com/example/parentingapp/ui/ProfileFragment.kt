1ca78ccba1b4cd511d360d93db8398dfaa8cf208
Silvia Prada Aprilia <silviaprada0904@gmail.com>
2023-06-15 02:32:32 +0700

    Add Profile Photo from Firestore

--- app/src/main/java/com/example/parentingapp/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/parentingapp/ui/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.example.parentingapp.ui
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
@@ -11,11 +12,13 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.parentingapp.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileFragment : Fragment() {
 
@@ -49,6 +52,9 @@ class ProfileFragment : Fragment() {
                     binding.username.text = it.data?.get("name") as String
                     binding.email.text = it.data?.get("class") as String
                     binding.tvParent.text = it.data?.get("email") as String
+                    Glide.with(this)
+                        .load(it.data?.get("profile"))
+                        .into(binding.avatar)
                 } else {
                     Log.d("failed", "Data doesn't exist")
                 }
@@ -63,14 +69,19 @@ class ProfileFragment : Fragment() {
             return
         }
 
-        binding.clLanguage.setOnClickListener {
-            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
-        }
-        binding.clTheme.setOnClickListener {
-            startActivity(Intent(requireActivity(), SettingActivity::class.java))
-        }
-        binding.btnLogout.setOnClickListener {
-            signOut()
+        binding.apply {
+            clLanguage.setOnClickListener {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+            }
+            clTheme.setOnClickListener {
+                startActivity(Intent(requireActivity(), SettingActivity::class.java))
+            }
+            clAccessbility.setOnClickListener {
+                startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+            }
+            btnLogout.setOnClickListener {
+                signOut()
+            }
         }
     }
 
