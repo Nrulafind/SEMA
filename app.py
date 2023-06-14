92ab6582e28a8a3d5fe877e1acbc646f23551591
Nrulafind <79900731+Nrulafind@users.noreply.github.com>
2023-06-14 14:31:20 +0700

    Update another app.py

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import requests
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import tensorflow as tf
+import ast
 
 app = Flask(__name__)
 CORS(app)
-#model_url = 'https://storage.googleapis.com/mlmodelsave/model.json'
+# model_url = 'https://storage.googleapis.com/mlmodelsave/model.json'
 model_path = 'score_model.h5'
 # if not os.path.exists(model_path):
 #     response = requests.get(model_url)
@@ -18,15 +19,14 @@ model = tf.keras.models.load_model(model_path)
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    input_data = request.json
-    if not isinstance(input_data, dict):
-        return jsonify({'message': 'Invalid JSON payload. Expected a dictionary.'}), 400
-    
-    input_data = input_data.get('input_data')
-    if input_data is None or len(input_data) != 4:
-        return jsonify({'message': 'input_data must contain 4 values'}), 400
-    
+    input_data_str = request.form.get('input_data')
     try:
+        print(input_data_str)
+        input_data = ast.literal_eval(input_data_str)
+        print(input_data)
+        if not isinstance(input_data, list) or len(input_data) != 4:
+            return jsonify({'message': 'input_data must contain 4 values in the format [80.5, 90.5, 1.0, 0.1]'}), 400
+
         input_tensor = tf.convert_to_tensor([input_data], dtype=tf.float64)
         output_tensor = model.predict(input_tensor)
         results = output_tensor.tolist()
